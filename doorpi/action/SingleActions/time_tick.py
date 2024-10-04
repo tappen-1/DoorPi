be4b29b257f8c588d658a5b919e1e0549fb7b252
tappen-1 <steven@neuhaus-elbe.de>
2024-10-04 13:45:29 +0200

    Anpassungen time_tick.py

--- doorpi/action/SingleActions/time_tick.py
+++ doorpi/action/SingleActions/time_tick.py
@@ -2,23 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import logging
-logger = logging.getLogger(__name__)
-logger.debug("%s loaded", __name__)
-
-from doorpi.action.base import SingleAction
 import time
 import datetime
 import doorpi
+from doorpi.action.base import SingleAction
+
+logger = logging.getLogger(__name__)
+logger.debug("%s loaded", __name__)
 
 MINUTE_RANGE = range(0, 60)
 HOUR_RANGE = range(0, 23)
 
+# Deklariere die globale Variable korrekt
 last_time_tick_second = 0
 
 def destroy_time_tick():
     doorpi.DoorPi().event_handler.unregister_source(__name__, True)
 
 def time_tick(last_tick):
+    global last_time_tick_second  # Stelle sicher, dass dies am Anfang der Funktion steht
 
     timestamp_now = time.time()
     timestamp_past = last_time_tick_second
@@ -28,60 +30,60 @@ def time_tick(last_tick):
 
     if datetime_now.year != datetime_past.year:
         doorpi.DoorPi().event_handler('OnTimeYear', __name__)
-        if datetime_now.year % 2 == 0: 
+        if datetime_now.year % 2 == 0:
             doorpi.DoorPi().event_handler('OnTimeYearEvenNumber', __name__)
-        else: 
+        else:
             doorpi.DoorPi().event_handler('OnTimeYearUnevenNumber', __name__)
 
     if datetime_now.month != datetime_past.month:
         doorpi.DoorPi().event_handler('OnTimeMonth', __name__)
-        if datetime_now.month % 2 == 0: 
+        if datetime_now.month % 2 == 0:
             doorpi.DoorPi().event_handler('OnTimeMonthEvenNumber', __name__)
-        else: 
+        else:
             doorpi.DoorPi().event_handler('OnTimeMonthUnevenNumber', __name__)
 
     if datetime_now.day != datetime_past.day:
         doorpi.DoorPi().event_handler('OnTimeDay', __name__)
-        if datetime_now.day % 2 == 0: 
+        if datetime_now.day % 2 == 0:
             doorpi.DoorPi().event_handler('OnTimeDayEvenNumber', __name__)
-        else: 
+        else:
             doorpi.DoorPi().event_handler('OnTimeDayUnevenNumber', __name__)
 
     if datetime_now.hour != datetime_past.hour:
         doorpi.DoorPi().event_handler('OnTimeHour', __name__)
-        if datetime_now.hour % 2 == 0: 
+        if datetime_now.hour % 2 == 0:
             doorpi.DoorPi().event_handler('OnTimeHourEvenNumber', __name__)
-        else: 
+        else:
             doorpi.DoorPi().event_handler('OnTimeHourUnevenNumber', __name__)
 
         for hour in HOUR_RANGE:
-            if hour == datetime_now.hour: 
+            if hour == datetime_now.hour:
                 doorpi.DoorPi().event_handler(f'OnTimeHour{hour}', __name__)
 
     if datetime_now.minute != datetime_past.minute:
         doorpi.DoorPi().event_handler('OnTimeMinute', __name__)
-        if datetime_now.minute % 2 == 0: 
+        if datetime_now.minute % 2 == 0:
             doorpi.DoorPi().event_handler('OnTimeMinuteEvenNumber', __name__)
-        else: 
+        else:
             doorpi.DoorPi().event_handler('OnTimeMinuteUnevenNumber', __name__)
 
         for minute in MINUTE_RANGE:
-            if minute == datetime_now.minute: 
+            if minute == datetime_now.minute:
                 doorpi.DoorPi().event_handler(f'OnTimeMinute{minute}', __name__)
 
-        if datetime_now.minute % 5 == 0: 
+        if datetime_now.minute % 5 == 0:
             doorpi.DoorPi().event_handler('OnTimeMinuteEvery5', __name__)
 
     if datetime_now.second != datetime_past.second:
         doorpi.DoorPi().event_handler('OnTimeSecond', __name__)
-        if datetime_now.second % 2 == 0: 
+        if datetime_now.second % 2 == 0:
             doorpi.DoorPi().event_handler('OnTimeSecondEvenNumber', __name__)
-        else: 
+        else:
             doorpi.DoorPi().event_handler('OnTimeSecondUnevenNumber', __name__)
 
+    # Aktualisiere die globale Variable hier
     last_time_tick_second = timestamp_now
-    global last_time_tick_second
-
+    
     return True
 
 def get(parameters):
